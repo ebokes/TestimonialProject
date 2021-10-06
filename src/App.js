7feb3f2dd4207ecda212d32ef8228ce572f62a59
johnpersie <83553199+johnpersie@users.noreply.github.com>
2021-10-06 01:06:21 +0100

    Intital commit for Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header className=""/>
       <h1>Application in progress...</h1>
     </div>
   );
