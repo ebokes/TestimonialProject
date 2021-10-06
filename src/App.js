d3687ae0025907ddf32f2754377142fb70670dec
Buokem <talk2chibu@gmail.com>
2021-10-06 09:03:10 +0100

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Application in progress...</h1>
+      <Navbar />
     </div>
   );
 }
