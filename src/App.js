30655ff6d4bf8d898533bfc24c23f8abf6556a37
johnpersie <83553199+johnpersie@users.noreply.github.com>
2021-10-12 17:23:15 +0100

    Creating the Header for the testimonial Page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <Header className=""/>
-      <h1>Application in progress...</h1>
+      <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,33 @@
-import React, { Fragment } from 'react'
+import React from 'react';
+import logo from '../logo.png'
 
 const Header = () => {
     return (
-        <Fragment>
-            
-        </Fragment>
+        
+        <header className='container'>    
+            <img src={logo} className='header-logo' alt="logo"/>
+                <nav className='header navbar'>    
+                    <ul>
+                        <li>
+                            <a href="/about-us">About Us</a>
+                        </li>
+                        <li>
+                            <a href="/stories">Stories</a>
+                        </li>
+                        <li>
+                            <a href="/contact">Contact</a>
+                        </li>
+                        <li>
+                            <a href="/log-in">Log In</a>
+                        </li>
+                        <li>
+                            <a href="/sign up" className='sign-up'>
+                                Sign up
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+        </header>
     )
 }
 
