2da176ea0379517fb372e376204cfe8f9250d0f3
Buokem <talk2chibu@gmail.com>
2021-10-13 06:48:04 +0100

    Created the story page 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
+
+import Header from "./components/Header";
 import LandingPage from "./components/LandingPage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import StoryPage1 from "./components/StoryPage1";
+// import StoryPage2 from "./components/StoryPage2";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Navbar />
       <LandingPage />
-      {/* <CallToAction /> */}
+      <StoryPage1 />
+      {/* <StoryPage2 /> */}
       <Footer />
     </div>
   );
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,16 +4,19 @@ import img1 from "../img/Testimonial image 1.png";
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <h1 className="amazing">
-        <span style={{ display: "block" }}>Amazing</span> Experiences from Our
-        Wonderful Customers
-      </h1>
-      <p className="here">
-        Here is what customers and vendors are saying about us, you can share
-        your stories with us too.
-      </p>
-      <div className="elipse"></div>
-      <img className="testimonial-img-1" src={img1} alt="testimonial" />
+      <div className="left">
+        <h1 className="amazing">
+          <span style={{ display: "block" }}>Amazing</span> Experiences from Our
+          Wonderful Customers
+        </h1>
+        <p className="here">
+          Here is what customers and vendors are saying about us, you can share
+          your stories with us too.
+        </p>
+      </div>
+      <div className="elipse">
+        <img className="testimonial-img-1" src={img1} alt="testimonial" />
+      </div>
     </div>
   );
 };
